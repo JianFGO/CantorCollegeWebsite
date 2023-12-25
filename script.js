9458acaf0854f8737275b9ee290854afe81fccbd
Jian lin <JianLin.L.He@student.shu.ac.uk>
2023-12-25 21:44:28 +0000

    fgtyrcv

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// scripts.js file content
 let slideIndex = 0;
+showSlides();
 
 function showSlides() {
     let slides = document.getElementsByClassName("mySlides");
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].classList.remove("active");
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 2 seconds (2000 milliseconds)
+    slides[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 5000); 
 }
 
-showSlides();
